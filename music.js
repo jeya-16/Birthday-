fd35bcdbd4f4e3eb3e7dfbbab7e9e85e79ef7306
jeya-16 <jeyajd5@gmail.com>
2026-08-02 14:40:57 +0530

    Update music.js

--- music.js
+++ music.js
@@ -2,43 +2,94 @@ let bgm = new Audio("Vaseegara.mp3");
 
 bgm.loop = true;
 
+
+// saved song time
 let savedTime = localStorage.getItem("songTime");
 
 if(savedTime){
     bgm.currentTime = savedTime;
 }
 
-let isPlaying = localStorage.getItem("songPlaying");
 
-if(isPlaying === "true"){
+// play button
+function playMusic(){
+
     bgm.play();
+
+    localStorage.setItem("songPlaying","true");
+
 }
 
 
-setInterval(function(){
+// ON / OFF button
+
+function toggleMusic(){
+
+    let btn = document.getElementById("musicBtn");
+
+
+    if(bgm.paused){
+
+        bgm.play();
+
+        localStorage.setItem("songPlaying","true");
+
+
+        if(btn){
+            btn.innerHTML="🔇 OFF";
+        }
+
+
+    }else{
+
+
+        bgm.pause();
+
+        localStorage.setItem("songPlaying","false");
+
+
+        if(btn){
+            btn.innerHTML="🎵 ON";
+        }
+
+    }
+
+}
+
+
+
+// save current time
+
+setInterval(()=>{
+
 
 if(!bgm.paused){
 
-localStorage.setItem("songTime", bgm.currentTime);
+localStorage.setItem(
+"songTime",
+bgm.currentTime
+);
 
 }
+
 
 },1000);
 
 
-function playMusic(){
+
+// next page open aana continue
+
+window.addEventListener("load",()=>{
+
+
+let status = localStorage.getItem("songPlaying");
+
+
+if(status === "true"){
 
 bgm.play();
 
-localStorage.setItem("songPlaying","true");
-
 }
 
 
-function pauseMusic(){
-
-bgm.pause();
-
-localStorage.setItem("songPlaying","false");
-
-}
+});
