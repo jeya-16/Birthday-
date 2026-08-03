0a1eaf11aa73f91a37b6eca1406c14a4fd98066d
jeya-16 <jeyajd5@gmail.com>
2026-08-03 12:33:55 +0530

    Create couple music.js

---
+++ couple music.js
@@ -0,0 +1,87 @@
+let bgm = new Audio("uyire.mp3");
+
+bgm.loop = true;
+
+// saved song time
+let savedTime = localStorage.getItem("coupleSongTime");
+
+if(savedTime){
+    bgm.currentTime = savedTime;
+}
+
+// play button
+function playMusic(){
+
+    bgm.play();
+
+    localStorage.setItem("coupleSongPlaying","true");
+
+}
+
+// ON / OFF button
+function toggleMusic(){
+
+    let btn = document.getElementById("musicBtn");
+
+    if(bgm.paused){
+
+        bgm.play();
+
+        localStorage.setItem("coupleSongPlaying","true");
+
+        if(btn){
+            btn.innerHTML="⏸️";
+        }
+
+    }else{
+
+        bgm.pause();
+
+        localStorage.setItem("coupleSongPlaying","false");
+
+        if(btn){
+            btn.innerHTML="▶️";
+        }
+
+    }
+
+}
+
+// save current time
+setInterval(()=>{
+
+    if(!bgm.paused){
+
+        localStorage.setItem(
+            "coupleSongTime",
+            bgm.currentTime
+        );
+
+    }
+
+},1000);
+
+// next page open aana continue
+window.addEventListener("load",()=>{
+
+    let status = localStorage.getItem("coupleSongPlaying");
+
+    let btn = document.getElementById("musicBtn");
+
+    if(status === "true"){
+
+        bgm.play();
+
+        if(btn){
+            btn.innerHTML="⏸️";
+        }
+
+    }else{
+
+        if(btn){
+            btn.innerHTML="▶️";
+        }
+
+    }
+
+});
