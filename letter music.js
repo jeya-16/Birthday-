29fd78a2f228a387b31f832ac5d62ce3db8fb7ed
jeya-16 <jeyajd5@gmail.com>
2026-08-03 16:28:31 +0530

    Create letter music.js

---
+++ letter music.js
@@ -0,0 +1,28 @@
+let bgm = new Audio("theri bus.mp3");
+
+bgm.loop = true;
+
+// Play / Pause
+function toggleMusic(){
+
+    let btn = document.getElementById("musicBtn");
+
+    if(bgm.paused){
+
+        bgm.play();
+
+        if(btn){
+            btn.innerHTML="💖";
+        }
+
+    }else{
+
+        bgm.pause();
+
+        if(btn){
+            btn.innerHTML="❤️";
+        }
+
+    }
+
+}
